fe4e1096d273e928a9bf81f809579b958798dc2e
mathguy-r <rohanmukherjee10@gmail.com>
2022-05-20 22:39:57 +0530

    edited test_app.py

--- test_app.py
+++ test_app.py
@@ -4,12 +4,16 @@ import time
 if __name__ == "__main__":
     st.title("This is a test Return Calculator")
     p = st.number_input("I want to invest (in INR)", min_value=1000, step=500)
-    f = st.radio("I want to do it",("One time","Monthly"))
+    f = st.radio("I want to do it",("One time","Monthly SIP"))
     r = st.number_input("Yearly Interest Rate", min_value=0.5, step=0.1)
     t = st.slider("I want to invest for (in years)", min_value=1,max_value=10)
     if st.button("Calculate"):
         if f == "Monthly":
-            p = p*12
+            r = r/1200
+            t = t*12
+            total = p*(1+r)*((1+r)**t - 1)/r
+        else:
+            total = p*(1+r/100)**t
         with st.spinner('computing ...'):
             time.sleep(1)
-            st.write(f"You will get INR {round(p*(1+r/100)**t,3)} in total")
+            st.write(f"You will get INR {round(total,3)} in total")
